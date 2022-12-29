7297af2c68ee0eb25085eed7fa89a2e38ad4f5e7
Mathys Reboux <mathysdevnode@gmail.com>
2022-12-29 23:52:39 +0100

    team rank cmd

--- index.js
+++ index.js
@@ -1,16 +1,40 @@
 // Require the necessary discord.js classes
-const { Client, Events, GatewayIntentBits } = require('discord.js');
-const { token } = require('./config.json');
+const { Client, GatewayIntentBits } = require('discord.js');
+const { botToken } = require('./config.json');
+const { htbToken } = require('./config.json');
+const fetch=require("node-fetch");
+
+async function getTeamRank(teamID){
+	//we fetch the htb api to get team data
+	let req=await fetch(`https://www.hackthebox.com/api/v4/rankings/team/ranking_bracket/${teamID}`, {
+  "headers": {
+    "accept": "application/json, text/plain, */*",
+    "authorization": `Bearer ${htbToken}`,
+  },
+  "body": null,
+  "method": "GET"
+});
+	let res=await req.json()
+	return res.data.rank
+}
 
 // Create a new client instance
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client({ intents: [GatewayIntentBits.Guilds,GatewayIntentBits.MessageContent,GatewayIntentBits.GuildMessages] });
 
 // When the client is ready, run this code (only once)
-// We use 'c' for the event parameter to keep it separate from the already defined 'client'
-client.once(Events.ClientReady, c => {
-	console.log(`Ready! Logged in as ${c.user.tag}`);
+client.on("ready",() => {
+	console.log(`Ready! Logged in as ${client.user.tag}`);
 });
 
-// Log in to Discord with your client's token
-client.login(token);
+//messageDetection
+client.on("messageCreate",async message=>{
 
+	//teamRank
+	if(message.content=="give team rank pls sir"){
+		let rank=await getTeamRank(5128)
+		await message.channel.send("We're #"+rank)
+	}
+})
+
+// Log in to Discord with your client's token
+client.login(botToken);
