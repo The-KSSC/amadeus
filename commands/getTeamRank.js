e089f34951965828c48df7dac785fef2d21c5f59
Kurisu Makise <ilcaneeunanimale@gmail.com>
2022-12-30 04:08:08 +0100

    make it more elegant

--- commands/getTeamRank.js
+++ commands/getTeamRank.js
@@ -2,31 +2,26 @@
 const fetch=require("node-fetch");
 const { htbToken } = require('../config.json');
 
+const { SlashCommandBuilder } = require('discord.js');
 
-module.exports={
-    name: "getTeamRank",
-    description: "returns team rank",
+module.exports = {
+        data: new SlashCommandBuilder()
+            .setName('team')
+            .setDescription('Returns team rank.'),
+        async execute(interaction) {
+            //fetch the htb api to get team data
+            let req=await fetch("https://www.hackthebox.com/api/v4/rankings/team/ranking_bracket/5128", {
+                "headers": {
+                "accept": "application/json, text/plain, */*",
+                "authorization": `Bearer ${htbToken}`,
+                },
+                "body": null,
+                "method": "GET"
+            });
+            let res=await req.json()
+            let rank= res.data.rank
+                
+            await interaction.reply('We\'re #'+rank);
+        },
+};
 
-    async run(client){
-        //look for the correct command
-        client.on("messageCreate",async message=>{
-            if(message.content=="getTeamRank"){
-                //fetch the htb api to get team data
-                let req=await fetch("https://www.hackthebox.com/api/v4/rankings/team/ranking_bracket/5128", {
-                    "headers": {
-                    "accept": "application/json, text/plain, */*",
-                    "authorization": `Bearer ${htbToken}`,
-                    },
-                    "body": null,
-                    "method": "GET"
-                });
-                let res=await req.json()
-                let rank= res.data.rank
-                //send the message
-                message.channel.send("We're #"+rank)
-            }
-        })
-
-    }
-
-}
--- index.js
+++ index.js
@@ -1,23 +1,66 @@
 // Require the necessary discord.js classes
-const { Client, GatewayIntentBits } = require('discord.js');
+const fs = require("fs")
+const path = require("path")
+const fetch = require("node-fetch");
 const { botToken,htbToken } = require('./config.json');
-const fetch=require("node-fetch");
-const fs=require("fs")
+const { Client, Events, Collection, GatewayIntentBits } = require('discord.js');
 
 // Create a new client instance
-const client = new Client({ intents: [GatewayIntentBits.Guilds,GatewayIntentBits.MessageContent,GatewayIntentBits.GuildMessages] });
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+/*const client = new Client({ intents: [GatewayIntentBits.Guilds,GatewayIntentBits.MessageContent,GatewayIntentBits.GuildMessages] }); Don't need? */
 
 // When the client is ready, run this code (only once)
 client.on("ready",() => {
 	console.log(`Ready! Logged in as ${client.user.tag}`);
 });
 
+//collection of commands: 
+//https://discord.js.org/#/docs/collection/main/class/Collection
+client.commands = new Collection();
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for(const file of commandFiles){
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+    //Set a new item in the Collection with the key as the command name
+    //and the value as the exported module
+    if('data' in command && 'execute' in command){
+        client.commands.set(command.data.name, command);
+    }else{
+        console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+    }
+}
+
+//Executing commands
+client.on(Events.InteractionCreate, async interaction => {
+	if (!interaction.isChatInputCommand()) return;
+
+	const command = interaction.client.commands.get(interaction.commandName);
+
+	if (!command) {
+		console.error(`No command matching ${interaction.commandName} was found.`);
+		return;
+	}
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+	}
+});
+
+
+
+/*
 //messageDetection
 fs.readdir("./commands/", (err, files) => {
 	files.forEach(filename=>{
 		require(`./commands/${filename}`).run(client)
 	})
 })
+*/
 
 // Log in to Discord with your client's token
 client.login(botToken);
